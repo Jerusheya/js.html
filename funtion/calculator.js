5c071e053322768b99adc837ed88416fdbb21de3
Jerusheya <jerusheya2002@gmail.com>
2021-11-26 23:43:31 +0530

    added calculator

--- funtion/calculator.js
+++ funtion/calculator.js
@@ -1,33 +1,23 @@
-//on submitting a form
-//goal1:get the two input values
-//goal2:add the two input values
-//goal3:display the output
+console.log("jeru");
 
-function addoption(a,b){
-    let a= parseInt(document.getElementById(num1).value);
-    let b= parseInt(document.getElementById(num2).value);
-    document.getElementById("result").value=a+b;
-}
-// addoption(a,b);
+function calculate() {
+    event.preventDefault();
+    let a=parseFloat(document.getElementById("num1").value);
+    let b=parseFloat(document.getElementById("num2").value);
+    let c=document.getElementById("sign").value;
 
-function suboption(a,b){
-    let a= parseInt(document.getElementById(num1).value);
-    let b= parseInt(document.getElementById(num2).value);
-    document.getElementById("result").value=a-b;
-}
-// suboption(a,b);
 
-function muloption(a,b){
-    let a= parseInt(document.getElementById(num1).value);
-    let b= parseInt(document.getElementById(num2).value);
-    document.getElementById("result").value=a*b;
-}
-// muloption(a,b);
+    if(c=="+"){
+        document.getElementById("result").value=a+b;
 
-function divoption(a,b){
-    let a= parseInt(document.getElementById(num1).value);
-    let b= parseInt(document.getElementById(num2).value);
-    document.getElementById("result").value=a/b;
-}
-// divoption(a,b);
-
+    };
+    if(c=="-"){
+        document.getElementById("result").value=a-b;
+    };
+    if(c=="*"){
+        document.getElementById("result").value=a*b;
+    };
+    if(c=="/"){
+        document.getElementById("result").value=a/b;
+    }
+   }
